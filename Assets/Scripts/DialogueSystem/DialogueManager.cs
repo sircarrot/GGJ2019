5f32fee40743067d21b20a4e419a8a19128f8b09
Royce Santo <roycesanto@yahoo.com>
2019-01-26 02:03:50 +0800

    Dialogue ScriptableObject

---
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CarrotPack;
+
+public class DialogueManager : MonoBehaviour, IManager
+{
+    [SerializeField] private DialogueScriptableObject DialogueScriptableObject;
+
+    public void InitializeManager()
+    {
+        DialogueScriptableObject.InitializeDictionary();
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/DialogueSystem/DialogueScriptableObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/DialogueScriptableObject")]
+public class DialogueScriptableObject : ScriptableObject {
+
+    public List<Dialogue> dialogueList = new List<Dialogue>();
+
+    public Dictionary<string, string> dialogueDictionary = new Dictionary<string, string>();
+
+    public void InitializeDictionary()
+    {
+        foreach(Dialogue dialogue in dialogueList)
+        {
+            if(dialogue.id == "" || dialogue.id == null)
+            {
+                Debug.LogWarning("No dialogue id!");
+                continue;
+            }
+            dialogueDictionary.Add(dialogue.id, dialogue.dialogue);
+        }
+    }
+}
+
+[System.Serializable]
+public class Dialogue
+{
+    public string id;
+    public string dialogue;
+}
