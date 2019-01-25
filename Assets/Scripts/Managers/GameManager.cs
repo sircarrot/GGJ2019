2898777f6e565290dda5e89d21064b941f121de6
Royce Santo <roycesanto@yahoo.com>
2019-01-26 02:24:25 +0800

    Manager Skeleton

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CarrotPack;
+
+public class GameManager : MonoBehaviour, IManager
+{
+    public void InitializeManager()
+    {
+    }
+
+
+
+
+    public void ChangeScene()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CarrotPack;
+
+public class InputManager : MonoBehaviour, IManager {
+
+    public void InitializeManager()
+    {
+    }
+    
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
