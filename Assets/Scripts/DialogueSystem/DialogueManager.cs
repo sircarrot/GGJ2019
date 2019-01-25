94e7a66203fc3af32e7def1b235e0c3929851386
Royce Santo <roycesanto@yahoo.com>
2019-01-26 02:21:35 +0800

    CSV reader for dialogue sequence

--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -5,12 +5,35 @@ using CarrotPack;
 
 public class DialogueManager : MonoBehaviour, IManager
 {
-    [SerializeField] private DialogueScriptableObject DialogueScriptableObject;
+    [SerializeField] private TextAsset DialogueCSV;
+    private Dictionary<string, string> dialogueDictionary = new Dictionary<string, string>();
+
+    private void PopulateDictionary()
+    {
+        string[] records = DialogueCSV.text.Split('\n');
+        int recordsSize = records.Length;
+
+        foreach(string record in records)
+        {
+            string[] data = record.Split(',');
+
+            string id = data[0].Trim(' ', ',');
+            string dialogue = "";
+            for(int i = 1; i < data.Length; ++i)
+            {
+                dialogue += data[i];
+            }
+            dialogue = dialogue.Trim('"', ' ');
+
+            dialogueDictionary.Add(id, dialogue);
+        }
+    }
 
     public void InitializeManager()
     {
-        DialogueScriptableObject.InitializeDictionary();
+        PopulateDictionary();
     }
+
 
 
 
