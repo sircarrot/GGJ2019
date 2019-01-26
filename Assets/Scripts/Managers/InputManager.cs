46a4b04b6162546c56aa824701cb92193d46b9dd
Royce Santo <roycesanto@yahoo.com>
2019-01-26 09:49:58 +0800

    Input Manager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -9,30 +9,30 @@ public class InputManager : MonoBehaviour, IManager {
 
     public void InitializeManager()
     {
+        if (gameManager == null) { gameManager = Toolbox.Instance.GetManager<GameManager>(); }
     }
     
-
 	// Update is called once per frame
 	void Update ()
     {
 		if(Input.GetKey(KeyCode.Space))
         {
-
+            gameManager.ReceiveInput(KeyCode.Space);
         }
 
         if(Input.GetKey(KeyCode.A))
         {
-
+            gameManager.ReceiveInput(KeyCode.A);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-
+            gameManager.ReceiveInput(KeyCode.D);
         }
 
         if (Input.GetKey(KeyCode.F)) // Interact
         {
-
+            gameManager.ReceiveInput(KeyCode.F);
         }
     }
 }
