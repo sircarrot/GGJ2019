ab9db952bf0956285da1215b5896762f917f2de0
ykw815 <ykw815@live.hk>
2019-01-26 19:15:15 +0800

    added PlayerLauncher logic

---
+++ Assets/Scripts/PlayerLauncher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CarrotPack;
+
+public class PlayerLauncher : MonoBehaviour 
+{
+	public Vector2 launchForce;
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		var otherRigidbody = other.GetComponent<Rigidbody2D>();
+		if(otherRigidbody != null)
+		{
+			otherRigidbody.velocity = Vector2.zero;
+			otherRigidbody.AddForce(launchForce);
+		}
+	}
+}
