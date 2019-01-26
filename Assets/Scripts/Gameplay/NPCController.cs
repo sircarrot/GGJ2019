81a0c9d08e7adf05042f9488fae4901cc297bb64
Royce Santo <roycesanto@yahoo.com>
2019-01-26 22:06:36 +0800

    NPC Prefab

---
+++ Assets/Scripts/Gameplay/NPCController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour {
+
+    public string name;
+    public int currentDialogueSequence = 0;
+    public int loopSequenceStart = 4;
+
+}
