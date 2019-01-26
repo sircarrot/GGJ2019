f083e2b523ad2afcba52f529d48e294862789aa8
ykw815 <ykw815@live.hk>
2019-01-26 23:00:04 +0800

    added ParallaxObject

---
+++ Assets/Scripts/ParallaxObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParallaxObject : MonoBehaviour 
+{
+	private Image image;
+    private Transform cameraTransform;
+	public float moveSpeed = 1f;
+	private void Start()
+	{
+		this.image = this.GetComponent<Image>();
+        cameraTransform =  GameObject.Find("Main Camera").transform;
+	}
+	private void Update()
+	{
+		this.image.rectTransform.localPosition = this.image.rectTransform.localPosition.WithX(cameraTransform.position.x * this.moveSpeed);
+	}
+}
